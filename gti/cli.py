b976031098093cfbf2228aec4f99257a06a19848
Almog Ben Chen <99953268+Almogbc@users.noreply.github.com>
2025-08-14 15:20:56 +0300

    argument parsing and repo init

--- gti/cli.py
+++ gti/cli.py
@@ -1,3 +1,21 @@
+import argparse
+
 def main():
-    print("Hello, World!")
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='GTI Command Line Interface')
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init', help='Initialize GTI repository')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args):
+    print(args)
 
